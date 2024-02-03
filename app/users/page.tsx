a6b17086014b81e81e0296e4ba522b859dd9a8ea
Ashim Rai <ashrai14691@gmail.com>
2024-02-03 23:32:39 +0545

    Fetching Data from Server Component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+interface User {
+  id: number;
+  firstName: string;
+}
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+// Fetching data in server component to avoid the additional roundtrip in server if we do it in client component
+
+// Define the ApiResponse type because it contains User[]
+interface ApiResponse {
+  users: User[];
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://dummyjson.com/users", { cache: "no-store" });
+  const userApiResponse: ApiResponse = await res.json();
+  const users: User[] = userApiResponse.users.map((user) => {
+    return { id: user.id, firstName: user.firstName };
+  });
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.firstName}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
