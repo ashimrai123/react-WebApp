23f7d27ef7f5c1e637e7441e14d6b48a0ef457c7
Ashim Rai <ashrai14691@gmail.com>
2024-02-04 13:26:19 +0545

    basic tailwind and daisyUI

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 interface User {
   id: number;
   firstName: string;
+  email: string;
 }
 
 // Fetching data in server component to avoid the additional roundtrip in server if we do it in client component
@@ -15,17 +16,27 @@ const UsersPage = async () => {
   const res = await fetch("https://dummyjson.com/users", { cache: "no-store" });
   const userApiResponse: ApiResponse = await res.json();
   const users: User[] = userApiResponse.users.map((user) => {
-    return { id: user.id, firstName: user.firstName };
+    return { id: user.id, firstName: user.firstName, email: user.email };
   });
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.firstName}</li>
-        ))}
-      </ul>
+      <table className=" table table-zebra	table-md	">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.firstName}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
